d9c500c25551714b1994575e6092f75445a6ba5f
NeverMit <markovnikov96@mail.ru>
2023-11-19 13:39:33 +0500

    lr4

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,24 @@
 import java.util.Scanner;
-//variant 14
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter n");
-        int n = scanner.nextInt();
-        int[] array = new int[n];
-        int s = 0;
-        int firstPositiveIndex=0;
-        int lastPositiveIndex=0;
-        for (int i = 0; i < array.length; i++)
-            array[i] = scanner.nextInt();
-        int a=123;
-        for (int i = 0; i < array.length; i++)
-            System.out.print(array[i] + " ");
-        for(int i=0;i< array.length;i++)
-            if(array[i]>0) {
-                firstPositiveIndex=i;
-                break;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Введите количество вопросов для экзамена");
+        int a=scanner.nextInt();
+        if(a<=0){
+            System.out.println("Недопустимое значение");
+        }
+        else {
+            System.out.println("Введите количество вопросов, которые выучил студент");
+            int b=scanner.nextInt();
+            if(a<b) {
+                System.out.println("Недопустимое значение");
             }
-        for (int i= array.length-1;i>0;i--)
-            if(array[i]>0){
-                lastPositiveIndex=i;
-                break;
+            else if(a>=b){
+                if(b>=0.9*a) System.out.println("Студент сдал на 5");
+                else if(b>=0.75*a) System.out.println("Студент сдал на 4");
+                else if(b<0.75*a&&b>=0.5*a) System.out.println("Студент сдал на 3");
+                else System.out.println("Студент не сдал");
             }
-        for(int i=firstPositiveIndex+1;i<=lastPositiveIndex-1;i++)
-            s+=array[i];
-        System.out.println();
-        System.out.println(s);
-        System.out.println("hui");
-        System.out.println("pizda");
-        System.out.println("jopa");
+        }
     }
 }
